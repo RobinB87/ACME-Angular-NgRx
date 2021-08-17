14e8dc7a1d98279442f1649f817bdfc727376b55
Robin Bette <robinbette@hotmail.com>
2021-08-17 11:50:34 +0200

    * Install devtools extension * Add app state extension in product reducer for lazy loading features * Strongly type product state

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,10 +1,22 @@
 import { createAction, createReducer, on } from '@ngrx/store';
+import * as AppState from '../../state/app.state';
+import { Product } from '../product';
 
-export const productReducer = createReducer(
+export interface State extends AppState.State {
+  products: ProductState;
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+export const productReducer = createReducer<ProductState>(
   {
     showProductCode: true,
-  },
-  on(createAction('[Product] Toggle Product Code'), (state) => {
+  } as ProductState,
+  on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
     return {
       ...state,
       showProductCode: !state.showProductCode,

5972f22bcfa09449c8dff9e40932a12c5e2fce23
Robin Bette <robinbette@hotmail.com>
2021-08-17 12:16:30 +0200

    Strongly type user state

--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,13 +1,41 @@
-import { createAction, createReducer, on } from '@ngrx/store';
+import {
+  createAction,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  on,
+} from '@ngrx/store';
+import { User } from '../user';
 
-export const userReducer = createReducer(
-  {
-    maskUserName: false,
-  },
-  on(createAction('[User] Mask User Name'), (state) => {
+export interface UserState {
+  currentUser: User;
+  maskUserName: boolean;
+}
+
+const initialState: UserState = {
+  currentUser: null,
+  maskUserName: false,
+};
+
+export const userReducer = createReducer<UserState>(
+  initialState,
+  on(createAction('[User] Mask User Name'), (state): UserState => {
     return {
       ...state,
       maskUserName: !state.maskUserName,
     };
   })
+);
+
+// Selectors
+const getUserFeatureState = createFeatureSelector<UserState>('user');
+
+export const getCurrentUser = createSelector(
+  getUserFeatureState,
+  (user) => user.currentUser
+);
+
+export const getMaskUserName = createSelector(
+  getUserFeatureState,
+  (user) => user.maskUserName
 );

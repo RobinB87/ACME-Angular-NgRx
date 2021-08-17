6f8dd6e471daecd85f44022b4d813a627ff322b7
Robin Bette <robinbette@hotmail.com>
2021-08-17 13:44:23 +0200

    Strongly type actions

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -6,6 +6,7 @@ import {
   on,
 } from '@ngrx/store';
 import * as AppState from '../../state/app.state';
+import * as ProductActions from '../state/product.actions';
 import { Product } from '../product';
 
 export interface State extends AppState.State {
@@ -26,10 +27,34 @@ const initialState: ProductState = {
 
 export const productReducer = createReducer<ProductState>(
   initialState,
-  on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
+  on(ProductActions.toggleProductCode, (state): ProductState => {
     return {
       ...state,
       showProductCode: !state.showProductCode,
+    };
+  }),
+  on(ProductActions.setCurrentProduct, (state, action): ProductState => {
+    return {
+      ...state,
+      currentProduct: action.product,
+    };
+  }),
+  on(ProductActions.clearCurrentProduct, (state): ProductState => {
+    return {
+      ...state,
+      currentProduct: null,
+    };
+  }),
+  on(ProductActions.initializeCurrentProduct, (state): ProductState => {
+    return {
+      ...state,
+      currentProduct: {
+        id: 0,
+        productName: '',
+        productCode: 'New',
+        description: '',
+        starRating: 0,
+      },
     };
   })
 );

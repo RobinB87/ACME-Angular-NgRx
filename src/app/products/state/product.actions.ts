0885d6931089331ee9672370ea54bcf8732a9e10
Robin Bette <robinbette@hotmail.com>
2021-08-17 14:10:33 +0200

    Set up products API actions

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -1,6 +1,18 @@
 import { createAction, props } from '@ngrx/store';
 import { Product } from '../product';
 
+export const loadProducts = createAction('[Product] Load');
+
+export const loadProductsSuccess = createAction(
+  '[Product] Load Success',
+  props<{ products: Product[] }>()
+);
+
+export const loadProductsFailed = createAction(
+  '[Product] Load Fail',
+  props<{ error: string }>()
+);
+
 export const toggleProductCode = createAction('[Product] Toggle Product Code');
 
 export const setCurrentProduct = createAction(

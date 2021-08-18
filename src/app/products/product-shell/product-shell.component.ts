cf8e81078b38caf29c8ef87f56e5555b4ba126e3
Robin Bette <robinbette@hotmail.com>
2021-08-18 10:31:28 +0200

    Implement presentational component pattern

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,12 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { Product } from '../product';
+import {
+  getCurrentProduct,
+  getError,
+  getProducts,
+  getShowProductCode,
+  State,
+} from '../state/product.reducer';
+import * as ProductActions from '../state/product.actions';
+import { Observable } from 'rxjs';
 
 @Component({
-  templateUrl: './product-shell.component.html'
+  templateUrl: './product-shell.component.html',
 })
 export class ProductShellComponent implements OnInit {
+  products$: Observable<Product[]>;
+  errorMessage$: Observable<string>;
 
-  constructor() { }
+  selectedProduct$: Observable<Product>;
+  displayCode$: Observable<boolean>;
 
-  ngOnInit() {
+  constructor(private store: Store<State>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(ProductActions.loadProducts());
+    this.products$ = this.store.select(getProducts);
+    this.errorMessage$ = this.store.select(getError);
+    this.selectedProduct$ = this.store.select(getCurrentProduct);
+    this.displayCode$ = this.store.select(getShowProductCode);
+  }
+
+  checkChanged(): void {
+    this.store.dispatch(ProductActions.toggleProductCode());
+  }
+
+  newProduct(): void {
+    this.store.dispatch(ProductActions.initializeCurrentProduct());
+  }
+
+  productSelected(product: Product): void {
+    this.store.dispatch(
+      ProductActions.setCurrentProduct({ currentProductId: product.id })
+    );
   }
 }

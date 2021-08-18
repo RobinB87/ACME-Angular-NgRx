d8f28b5f51cd8e2ca3fd1a0eb36571c8da263d1e
Robin Bette <robinbette@hotmail.com>
2021-08-18 10:42:08 +0200

    Change direction on push

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,10 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { Product } from '../product';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  // With on push, unless a new input reference is passed,
+  // or a DOM event is raised, or you use an async pipe in the component's template (or it's children)
+  // the view will NOT be updated
 })
 export class ProductListComponent {
   pageTitle = 'Products';
